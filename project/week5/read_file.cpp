a2fd3e45f14755609927fd87337649176b59f5e9
Lazy-Gun <132676103+Lazy-Gun@users.noreply.github.com>
2025-09-30 15:14:21 +0100

    start of week5 assignment read from file and tokenise working on windows

--- project/week5/read_file.cpp
+++ project/week5/read_file.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <iomanip>
+#include<fstream>
 
 std::vector<std::string> tokenise(std::string csvLine, char separator){
 
@@ -35,15 +37,57 @@ std::vector<std::string> tokenise(std::string csvLine, char separator){
 
 int main(){
 
+/*       
+    // call tokenise function to read string of CSV 
         std::vector<std::string> tokens;
         std::string s = "01:24.9,ETH/BTC,OrderBookType::bid,0.02187308,7.44564869";
         tokens = tokenise(s, ',');
 
         for (std::string& t :tokens)
         {
+
             std::cout << t << std::endl;
 
+        } */
+
+    // Read in from file
+
+    std::ifstream csvFile{"crypto_dataset_with_errors.csv"};
+    std::string line;
+    std::vector<std::string> tokens;
+
+    if(csvFile.is_open()){
+
+        std::cout << "File is open" << std::endl;
+        while(std::getline(csvFile, line)){
+
+            std::cout << "Read line: "<< line << std::endl;
+
+            tokens = tokenise(line, ',');
+            if(tokens.size() != 5){
+                std::cout << "bad csv line" << std::endl;
+                continue;
+            }
+            try{
+                double price = std::stod(tokens[3]);
+                double amount = std::stod(tokens[4]);
+                std::cout << std::fixed << std::setprecision(8); 
+                std::cout << price <<" : " << amount << std::endl;
+            }catch(std::exception& e){
+                 std::cout << "Bad float" << tokens[3] << std::endl;
+                 std::cout << "Bad float" << tokens[4] << std::endl;
+                 continue;
+              
+            }
+
+
         }
+        csvFile.close();
+    }
+    else
+    {
+        std::cout << "Could not open file" << std::endl;
+    }
  
     return 0;
 }
